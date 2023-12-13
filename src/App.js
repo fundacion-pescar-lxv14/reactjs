a8a12e12ee315b4f41a88b07470e30526298b2d3
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-13 15:25:41 -0300

    instalar e importar iconos fontawesome-free

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 import logo from './logo.svg';
 import menu from './components/links.json';
+import social from './components/social.json';
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+// Font Awesome
+import 'fontawesome-free/css/all.min.css';
 // Componentes
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import { Social } from './components/Social';
 
 function App() {
   return (
@@ -19,6 +23,7 @@ function App() {
         title="ReactJS" 
         subtitle="create-react-app template">
       </Header>
+      <Social links={social}/>
       <Footer 
         author="Cristian Racedo" 
         copy="Fundacion Pescar" 
