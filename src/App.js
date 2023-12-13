27ede68ac30aea2f3aed731691e7b7c82f921aff
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-13 14:36:47 -0300

    instalar e importar bibliotecas de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 // Archivos externos
 import './App.css';
 import logo from './logo.svg';
+import menu from './components/links.json';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 // Componentes
 import Header from './components/Header';
+import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <Navigation brand="PescarShops" links={menu}/>
       <Header 
         logo={logo} 
         title="ReactJS" 
