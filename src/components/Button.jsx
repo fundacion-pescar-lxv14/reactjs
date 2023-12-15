c5fe7314267e3791cc0f51820b5627822a38ef57
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-15 16:39:20 -0300

    definir estado y metodos para componente button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,1 +1,12 @@
-export const Button = ({text}) => <button>{text}</button>
+import { useState } from "react"
+export const Button = ({text}) => {
+    // Declaracion Estado y Metodo de Manipulacion
+    const [click, setClick] = useState(),
+    // Funcion asociada a un Evento del Componente
+    handle = () => setClick(!click)
+    // Componente a renderizar en el DOM
+    return (
+    <button onClick={handle}>
+        {text} {click && "+1"}
+    </button>
+)}
