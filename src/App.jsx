3a70819a975efc9cfb6e22338666a2691731e409
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-15 16:39:37 -0300

    agregar objetos al array de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,28 @@ function App() {
     flexWrap: "wrap",
     justifyContent:"center"
   },
-  colStyle = { flex: "1 0 min(100%, 320px)" },
-  buttons = ['like', 'dislike', 'love'],
+  colStyle = {display: "grid", gap: ".5rem", flex: "1 0 min(100%, 320px)", textAlign: "left" },
+  buttons = ['like','dislike','love','angry'],
   images = [
     {
       title: "vite",
       img: viteLogo,
-      content: "Plantilla que simplifica la creacion de un proyecto con ReactJS"
+      content: "Plantilla que simplifica la creacion de un proyecto con ReactJS, VueJS, LitElement o VanillaJS con Typescript o Javascript"
     },
     {
       title: "react",
       img: reactLogo,
-      content: "Framework de JS para desarrollo de interfaces de usuario"
+      content: "Framework de JS creado por facebook para desarrollo de interfaces de usuario sencillas, escalables y de rapida ejecucion"
+    },
+    {
+      title: "ES6",
+      img: "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_js_official_icon_130509.png",
+      content: "Estandar de JS que define la sintaxis del lenguaje, incluyendo nuevas funcionalidades como clases, promesas, arrow functions, etc."
+    },
+    {
+      title: "JSX",
+      img: "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_xml_icon_130060.png",
+      content: "Extension de JS que permite escribir con una sintaxis similar HTML en JS debido a la implementacion de XML"
     }
   ]
   return (
