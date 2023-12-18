1e11ece47aee8c2c24286f90b65e43ed24f286c7
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-18 15:41:22 -0300

    deestructurar aplicacion en componentes

---
+++ src/components/CreateBoard.jsx
@@ -0,0 +1,9 @@
+export const CreateBoard = (rows = 3, cols = 3) => {
+    const board = []
+    for(let f = 0; f < rows; f++) {
+        const row = []
+        for(let c = 0; c < cols; c++) row.push("")
+        board.push(row)
+    }
+    return board
+}
