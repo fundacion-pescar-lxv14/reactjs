0f33532d4f76297fb00d8634b3f2e22ef885122e
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-18 15:13:31 -0300

    agregar actualizaciones del estado y verificacion de propiedades

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,52 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [state, setState] = useState({
-    currentPlayer: "X",
-    nextPlayer: "O",
-    board: [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
-    ],
-    winner: null
-  })
-  const { board, nextPlayer, currentPlayer } = state;
-  const change = () => {setState({
-    ...state,
-    currentPlayer: nextPlayer, 
-    nextPlayer: currentPlayer
-  })}
+  const [state, setState] = useState(),
+  createBoard = (rows = 3, cols = 3) => {
+    const board = []
+    for(let f = 0; f < rows; f++) {
+      const row = []
+      for(let c = 0; c < cols; c++) row.push("")
+      board.push(row)
+    }
+    return board
+  },
+  start = () => {
+    setState({
+      currentPlayer: "X",
+      nextPlayer: "O",
+      winner: null,
+      board: createBoard()
+    })
+  },
+  change = (e) => { 
+    const [ {board:b,currentPlayer:cP,nextPlayer:nP}, [f,c] ] = 
+    [state, e.target.id.split("-") ]
+    
+    e.target.innerHTML === "" ?
+    setState({
+      ...state,
+      currentPlayer: nP, 
+      nextPlayer: cP,
+      board: b.map((row, bf) => row.map((cell, bc) =>  
+        bf === Number(f) && bc === Number(c) ? cP : cell
+      ))
+    }) : 
+    alert("Esa posicion esta ocupada. Elija otra casilla")
+  }
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <ul>
-        <li>Turno del jugador {state.currentPlayer}</li>
-        <li>Siguente jugador {state.nextPlayer}</li>
+        <li>Turno del jugador {state?.currentPlayer}</li>
+        <li>Siguente jugador {state?.nextPlayer}</li>
       </ul>
-      { board.map((row, i) => 
-      <div key={i} className='row'>
-        { row.map((cell, i) => <button key={i} onClick={change}>{cell}</button> )}
+      { state?.board?.map((row, f) => 
+      <div key={f} className='row'>
+        { row.map((cell, c) => 
+          <button id={f+"-"+c} key={c} onClick={change}>{cell}</button> )}
       </div> )}
+      <button onClick={start}>Empezar</button>
     </>
   )
 }
