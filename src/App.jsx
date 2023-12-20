6bada6319fd923857c93b4476cef6f353e333973
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-20 15:43:51 -0300

    simplificar estructura de componentes y funciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,38 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-import { CreateBoard } from './components/CreateBoard'
+import { StartGame } from './controllers/StartGame'
+import { GetWinner } from './controllers/GetWinner'
+import { UpdateBoard } from './controllers/UpdateBoard'
 
 import { Score } from './components/Score'
 import { Board } from './components/Board'
 import { Action } from './components/Action'
 
 function App() {
-  const [state, setState] = useState(),
-  start = () => {
-    setState({
-      currentPlayer: "X",
-      nextPlayer: "O",
-      winner: null,
-      board: CreateBoard()
-    })
-  },
+  const [state, setState] = useState(StartGame()),
+  start = () => setState({...StartGame(), paused: false}),
+  end = () => setState({...state, paused: !state.paused}),
   setWinner = () => {
-    const { board:b, nextPlayer:nP } = state
-    for (let n = 0; n < 3; n++)(
-    (b[n][0] === b[n][1] && b[n][0] === b[n][2] && b[n][0] !== "") ||
-    (b[0][n]) === b[1][n] && b[0][n] === b[2][n] && b[0][n] !== "") || 
-    (n === 0 && b[n][n] === b[n+1][n+1] && b[n][n] === b[n+2][n+2] && b[n][n] !== "") || 
-    (n === 2 && b[n][n] === b[n-1][n-1] && b[n][n] === b[n-2][n-2] && b[n][n] !== "") ? 
-      setState({ ...state, winner: nP }) : 
-      false
+    const { board, winner, nextPlayer:nP } = state
+    winner === null && GetWinner(board) ? setState({...state, winner: nP}) : null
   },
   change = (e) => { 
-    const [ {board:b,currentPlayer:cP,nextPlayer:nP}, [f,c] ] = 
+    const [{board,winner,currentPlayer:cP,nextPlayer:nP},[f,c]] = 
     [state, e.target.id.split("-") ]
-    e.target.innerHTML === "" ?
-    setState({
-      ...state,
-      currentPlayer: nP, 
-      nextPlayer: cP,
-      board: b.map((row, bf) => row.map((cell, bc) =>  
-      bf === Number(f) && bc === Number(c) ? cP : cell
-      ))
-    }) : 
-    alert("Esa posicion esta ocupada. Elija otra casilla")
-    setWinner();
-  };
-  const actions = [{text:"Empezar", callback: start}, {text:"Finalizar", callback: start}]
-
-  // useEffect(() => { setWinner() }, [state])
+    if(winner === null){
+      e.target.innerHTML === "" ?
+      setState({
+        ...state,
+        currentPlayer: nP, 
+        nextPlayer: cP,
+        board: UpdateBoard(board,f,c,cP)
+      }) : 
+      alert("Esa posicion esta ocupada. Elija otra casilla")}
+    };
+  const actions = [{text:"Nueva Partida", callback: start}, {text: state.paused ? "Iniciar": "Pausar", callback: end}]
+  // Component Will Update
+  useEffect(() => { setWinner() }, [state])
   return (
     <>
       <h1>Tic Tac Toe</h1>
