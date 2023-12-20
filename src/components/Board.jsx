d1d1b3863535ba50a444438cf88bccd3cdb73810
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-20 15:43:36 -0300

    agregar estilos para botones

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,21 @@
-export const Board = ({board, callback}) => 
+const flexbox = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+}
+const btn = {
+    minHeight: "4rem",
+    minWidth: "4rem",
+    fontSize: "1rem",
+    padding: ".5rem",
+    margin: ".25rem"
+}
+export const Board = ({board, paused, callback}) => 
 board?.map((row, f) => 
-    <div key={f} className='row'>{ 
+    <div key={f} style={flexbox}>{ 
     row.map((cell, c) => 
-        <button id={f+"-"+c} key={c} onClick={callback}>
+        <button style={btn} id={f+"-"+c} key={c} onClick={callback} disabled={paused}>
             {cell}
         </button> 
         )}
