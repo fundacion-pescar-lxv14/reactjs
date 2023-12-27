9d2a5582e0e9b9cc739768b96d6705e78dd62d7a
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-12-27 16:16:17 -0300

    crear contexto para busquedas

---
+++ src/contexts/SearchContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useEffect } from "react";
+// Suscriptor del Contexto
+export const SearchContext = createContext();
+// Proveedor del Contexto
+export const SearchProvider = ({children}) => {
+    const [{VITE_URL:url, VITE_API_KEY:key},[results,setResults],[search,setSearch]] = [import.meta.env, useState(), useState({
+        text: "",
+        limit: 100,
+        offset: 0,
+        rating: "g",
+        lang: "es",
+        bundle: "low_bandwidth"
+    })],
+    Set = (k,v) => setSearch({...params, [k]:v })
+    useEffect(()=>{
+        const {text, limit, offset, rating, lang, bundle} = search;
+        text.length > 3 ? (
+            fetch(`http://${url}?api_key=${key}&q=${text}&limit=${limit}&offset=${offset}&rating=${rating}&lang=${lang}&bundle=${bundle}`)
+            .then(response => response.json())
+            .then(data => setResults(data))
+        ) : setResults("")
+    },[search])
+    return (
+    <SearchContext.Provider value={{results, search, Set, setSearch}}>
+        {children}
+    </SearchContext.Provider>
+)}
