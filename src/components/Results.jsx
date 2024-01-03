9b2400425759ad85be4d1a37ccdf69999aabc922
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-01-03 13:47:02 -0300

    actualizar estructura de resultados

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,16 @@
+import { useContext } from "react"
+import { SearchContext } from "../contexts/SearchContext"
 import { Post } from "./Post"
 
-export const Results = ({text, results}) => 
+export const Results = () => {
+    const {results, search:{text}} = useContext(SearchContext)
+    return(
     <main className="container row p-3 gap-2">
         <h2>{ results ? "Resultados de "+text : "No existen resultados" }</h2>{
-        results?.map((props,i) => 
+        results?.data?.map((props,i) => 
             <>
             <Post key={"article-"+i} {...props} />
-            { (i+1)%3 === 0 && <div key={"divider-"+i} className="col-12"></div> }
+            {(i+1)%3 === 0 && <div key={"divider-"+i} className="col-12"></div> }
             </> 
         )}
-    </main>
+    </main>)}
