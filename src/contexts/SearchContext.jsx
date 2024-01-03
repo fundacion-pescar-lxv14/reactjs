8970f5efeacc86edb4401a4e8a0342a441163b0f
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-01-03 13:34:40 -0300

    definir contexto para busqueda y resultados

--- src/contexts/SearchContext.jsx
+++ src/contexts/SearchContext.jsx
@@ -3,22 +3,22 @@ import { createContext, useState, useEffect } from "react";
 export const SearchContext = createContext();
 // Proveedor del Contexto
 export const SearchProvider = ({children}) => {
-    const [{VITE_URL:url, VITE_API_KEY:key},[results,setResults],[search,setSearch]] = [import.meta.env, useState(), useState({
+    const [{VITE_URL:url, VITE_API_KEY:key},[results,setResults],[search,setSearch]] = [import.meta.env, useState(null), useState({
         text: "",
-        limit: 100,
+        limit: 10,
         offset: 0,
         rating: "g",
         lang: "es",
         bundle: "low_bandwidth"
     })],
-    Set = (k,v) => setSearch({...params, [k]:v })
+    Set = (k,v) => setSearch({...search, [k]:v })
     useEffect(()=>{
         const {text, limit, offset, rating, lang, bundle} = search;
         text.length > 3 ? (
             fetch(`http://${url}?api_key=${key}&q=${text}&limit=${limit}&offset=${offset}&rating=${rating}&lang=${lang}&bundle=${bundle}`)
             .then(response => response.json())
             .then(data => setResults(data))
-        ) : setResults("")
+        ) : setResults(null)
     },[search])
     return (
     <SearchContext.Provider value={{results, search, Set, setSearch}}>
