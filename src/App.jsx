8eb3c50cbd41be6691a7af0b6c8b63c019cb0513
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-01-05 08:21:14 -0300

    implementar estructura principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import './App.css'
+import { TasksProvider } from "./hooks"
+import { TasksAdd, TasksFilter, TasksList } from "./components"
 
 function App() {
-
-}
+    return(
+    <TasksProvider>
+        <TasksAdd/>
+        <TasksFilter/>
+        <TasksList/>
+    </TasksProvider>
+)}
 
 export default App
