f7d78c2399c0b920f3f37bf92157253f67e294ed
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-01-12 17:17:00 -0300

    actualizar consulta de estados y solicitud http

--- src/hooks/TasksProvider.jsx
+++ src/hooks/TasksProvider.jsx
@@ -1,9 +1,9 @@
-import { createContext, useEffect, useState, useReducer } from 'react'
-import { TasksReducer, useRequest } from './';
+import { createContext, useContext, useEffect, useState, useReducer } from 'react'
+import { TasksReducer, EditContext, useRequest } from './';
 
 export const TasksContext = createContext([]);
 export const TasksProvider = ({children}) => {
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState([]);
     const [state, dispatch] = useReducer(TasksReducer, tasks);
     useEffect( ()=>{useRequest({callback: (e) => setTasks(e)})}, [])
     return(
--- src/hooks/useRequest.jsx
+++ src/hooks/useRequest.jsx
@@ -6,13 +6,14 @@ export const useRequest = ({
 }) => {
     const baseURL = import.meta.env.VITE_SERVER_URL
     const data = new URLSearchParams()
-    const object = {}
+    const object = {method}
     const URL = `${baseURL}/tasks${id?"/"+id:''}` 
     if (method === "POST" || method === "PUT"){
         Object.keys(params)?.map(key =>{
             if (params[key]!=null) data.append(key, params[key])
         })
-        object.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        object.method = method;
+        object.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
         object.body = data.toString()
     }
     return (
